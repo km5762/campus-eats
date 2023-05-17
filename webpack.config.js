4f440a2914bd2947fe7320a5c57ffb73800e1d0c
km5762 <km5762@DESKTOP-587P2TU>
2023-05-17 18:10:00 -0400

    Reorganized file structure to add public folder for static assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const pages = ["index", "school-page"];
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: pages.reduce((config, page) => {
     config[page] = `./src/scripts/${page}.ts`;
     return config;
@@ -22,6 +24,22 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./public/images",
+          to: "./images",
+          noErrorOnMissing: true,
+        },
+        {
+          from: "./public/styles",
+          to: "./styles",
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
   optimization: {
     splitChunks: {
       chunks: "all",
