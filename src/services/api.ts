79a5198842f35d4bdb4276c3289031a5e4aa6970
km5762 <kumyles@gmail.com>
2023-06-20 16:07:24 -0400

    Removed api routes and instead integrated queries directly into api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,65 +1,76 @@
+import { createClient } from "@supabase/supabase-js";
 import { DishData } from "../components/DishCard";
 import { LocationData } from "../components/LocationCard";
+import { Suggestion } from "../components/SearchBar";
 
-export default async function fetchSearch(query: string) {
+const supabaseUrl = "https://praaunntraqzwomikleq.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByYWF1bm50cmFxendvbWlrbGVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ1NDAzODMsImV4cCI6MjAwMDExNjM4M30.iy7rGNKGQ5HeK0xJhKN3OzXqbNnegkVVAic7rWZ-iXU";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default async function fetchSearch(search: string) {
   try {
-    const response = await fetch(
-      `/api/campus/search?query=${encodeURIComponent(query)}`
-    );
+    const { data, error } = await supabase
+      .from("campus")
+      .select()
+      .ilike("name", `%${search}%`);
 
-    if (!response.ok) {
-      // Handle HTTP errors
-      throw new Error(`HTTP error: ${response.status}`);
+    if (error) {
+      throw error;
     }
 
-    const suggestions = await response.text();
-    return JSON.parse(suggestions);
+    const searchData: Suggestion[] = data.map((suggestion: any) => ({
+      ...suggestion,
+    }));
+
+    return searchData;
   } catch (error) {
-    console.error("Error occurred during fetchSearch:", error);
-    throw error;
+    console.error(error);
+    return [];
   }
 }
 
 export async function fetchLocations(campusID: number) {
   try {
-    const response = await fetch(`/api/campus/${campusID}/locations`);
+    const { data, error } = await supabase.rpc("fn_get_locations_at", {
+      p_id: campusID,
+    });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
+    if (error) {
+      throw error;
     }
 
-    const locations = await response.json();
-
-    const locationData: LocationData[] = locations.map((location: any) => ({
+    const locationData: LocationData[] = data.map((location: any) => ({
       type: "location",
       ...location,
     }));
 
     return locationData;
   } catch (error) {
-    console.error("Error occurred during fetchLocations:", error);
-    throw error;
+    console.error(error);
+    return [];
   }
 }
 
 export async function fetchDishes(locationID: number) {
   try {
-    const response = await fetch(`/api/locations/${locationID}/dishes`);
+    const { data, error } = await supabase
+      .from("dish")
+      .select("*")
+      .eq("location_id", locationID);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
+    if (error) {
+      throw error;
     }
 
-    const dishes = await response.json();
-
-    const dishData: DishData[] = dishes.map((dish: any) => ({
+    const dishData: DishData[] = data.map((dish: any) => ({
       type: "dish",
       ...dish,
     }));
 
     return dishData;
   } catch (error) {
-    console.error("Error occurred during fetchDishes:", error);
-    throw error;
+    console.error(error);
+    return [];
   }
 }
