c90af9dbab3a6e8246972b440c10f8bc12d22f6d
km5762 <kumyles@gmail.com>
2023-06-21 22:46:25 -0400

    add new hooks folder

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import AuthButtons from "./AuthButtons";
-import { useSupabaseSession } from "../services/useSupabaseSession";
+import { useSupabaseSession } from "../hooks/useSupabaseSession";
 import { supabaseClient } from "../services/supabaseClient";
 
 export default function Index() {
