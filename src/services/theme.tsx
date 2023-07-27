0fc831b2c864717a21391165cbcd3ac4e53e60f7
km5762 <kumyles@gmail.com>
2023-07-27 17:07:37 -0400

    Globally disable touch ripple

--- src/services/theme.tsx
+++ src/services/theme.tsx
@@ -4,6 +4,13 @@ const theme = createTheme({
   typography: {
     fontFamily: "Raleway",
   },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableTouchRipple: true,
+      },
+    },
+  },
 });
 
 export default theme;
